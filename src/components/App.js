01b51998a35d94c0344dd5986d3512fddc995105
Evodio Garcia <evodiog@gmail.com>
2019-08-06 17:00:38 -0700

    creates boilerplate for react project with webpack

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='main-container'>
+        <h1>Start Here</h1>
+      </div>
+    )
+  }
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './styles/style.sass';
+
+render(
+  <App />,
+  document.getElementById('app')
+);
